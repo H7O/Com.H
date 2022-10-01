b2cdc2fe223200b0eb3f6ce8eae2831d81f5b8ad
Hussein Al Bayati <hussein@albayati.com>
2022-10-02 00:18:22 +0400

    #

--- Net6/Pdf/ExternalPdfConverter.cs
+++ Net6/Pdf/ExternalPdfConverter.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using Com.H.IO;
 using Com.H.Runtime.InteropServices;
 using Com.H.Shell;
